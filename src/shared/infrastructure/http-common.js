6814663120e2e1a5b5382af7bf42a1987b573bef
Rodrigo López Monroy <rodrigolopezu2@gmail.com>
2026-05-12 02:41:11 -0500

    hotfix: Change base URL for HTTP requests to production

--- src/shared/infrastructure/http-common.js
+++ src/shared/infrastructure/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const http = axios.create({
     // baseURL: import.meta.env.VITE_API_URL,
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://autoservice-api.ddns.net/',
     headers: { 'Content-type': 'application/json' }
 });
 
