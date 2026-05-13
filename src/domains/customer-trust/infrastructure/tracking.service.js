63db4f58f0bb99d0006dc4b98cd747d80940086d
rodrigolopezu2@gmail.com <rodrigolopezu2@gmail.com>
2026-05-13 00:41:28 -0500

    fix: change api route

--- src/domains/customer-trust/infrastructure/tracking.service.js
+++ src/domains/customer-trust/infrastructure/tracking.service.js
@@ -5,7 +5,7 @@ import axios from 'axios';
  */
 
 const publicHttp = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://autoservice-api.ddns.net/',
     headers: { 'Content-type': 'application/json' }
 });
 /**
--- src/shared/infrastructure/http-common.js
+++ src/shared/infrastructure/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://autoservice-api.ddns.net/',
     headers: { 'Content-type': 'application/json' }
 });
 
